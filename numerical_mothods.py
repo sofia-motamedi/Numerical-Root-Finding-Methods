2997ee8e55a38f9984ce473794977a1ceb332394
sofia-motamedi <s.motamedi04@gmail.com>
2026-09-12 17:05:01 +0330

    Update numerical_mothods.py

--- numerical_mothods.py
+++ numerical_mothods.py
@@ -1,1 +1,18 @@
+def bisection(f, a, b, tol, v0, theta, g):
+    if f(a, v0, theta, g) * f(b, v0, theta, g) >= 0:
+        raise ValueError(
+            "Bisection method requires opposite signs at the interval endpoints."
+        )
 
+    while abs(b - a) >= tol:
+        c = (a + b) / 2
+
+        if abs(f(c, v0, theta, g)) < tol:
+            return c
+
+        if f(a, v0, theta, g) * f(c, v0, theta, g) < 0:
+            b = c
+        else:
+            a = c
+
+    return (a + b) / 2
